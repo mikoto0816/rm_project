a3f77f82fafee7bd6ec6b172ddd8b69e27f26d3f
mikoto0816 <1582016243@qq.com>
2024-05-07 21:06:51 +0800

    添加通用返回结构体

--- project-user/api/user/user.go
+++ project-user/api/user/user.go
@@ -1,10 +1,14 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	common "rimomi.com/project-common"
+)
 
 type HandlerUser struct {
 }
 
 func (*HandlerUser) getCaptCha(ctx *gin.Context) {
-	ctx.JSON(200, "sucess")
+	result := &common.Result{}
+	ctx.JSON(200, result.Success("成功"))
 }
